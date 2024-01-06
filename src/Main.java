f667963bb2643229005f6aa58f05f5491d632d3a
Nikita <nikita.korshunov.95@mail.ru>
2024-01-06 22:57:01 +0500

    Coursework 1 - task from exam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String result = "";
+        String[] arr = {"2", "5", "8", "2", "3", "1", "5", "4", "7"};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i].equals(arr[j])) {
+                        arr[j] = null;
+                    }
+                }
+                result += arr[i];
+            }
+        }
+        System.out.println(result);
     }
 }
